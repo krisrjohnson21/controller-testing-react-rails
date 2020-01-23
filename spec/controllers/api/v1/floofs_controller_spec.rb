fa203b15b51f2c152beee35ad2550e0d919cf91b
Kris Johnson <krisrjohnson@gmail.com>
2020-01-23 10:24:02 -0500

    updated test suite

--- spec/controllers/api/v1/floofs_controller_spec.rb
+++ spec/controllers/api/v1/floofs_controller_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::FloofsController, type: :controller do
-  first_floof = Floof.create(
+  let!(:first_floof) { Floof.create(
     name: "Trogdor The Burninator",
     floofiness: 1,
     image: "https://lh5.googleusercontent.com/proxy/X3_Io8z31Fim6hdOC47CIAOM4iBAAJQ5XKa9jw14gwbgqdVjA-lc-dzyxKdkqWsn52jtOIkFc8fIAMz67e7NfUfiKF7VR30uZ--sFoHxvG8tt1upS23ecWnZZ2Y"
-  )
+  ) }
 
   describe "GET#show" do
     it "should return the floof and their floofiness" do
@@ -14,6 +14,7 @@ RSpec.describe Api::V1::FloofsController, type: :controller do
 
       expect(response.status).to eq 200
       expect(response.content_type).to eq("application/json")
+      
       expect(returned_json["floof"]["name"]).to eq "Trogdor The Burninator"
       expect(returned_json["floof"]["floofiness"]).to eq 1
     end
